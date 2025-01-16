bfa7734973609598df1f8eb5a22bd21c73eaa94d
Lekshmi Sreekumar <lekshmidevusreekumar@gmail.com>
2025-01-16 19:12:21 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<div class="menu-container" style="text-align:center">
+<h1>Student Management</h1>
+<a href="studentlogin.php" style="display:block";>Home</a>
+<a href="insertion.php" style="display:block";>Add Student Details</a>
+<a href="viewall.php" style="display:block";>View All Students</a>
+<a href="search.php" style="display:block";>Search a Student with Student Id</a>
+<a href="delete.php" style="display:block";>Delete a Student</a>
+<a href="update1.php" style="display:block";>Update Student Details</a>
+<a href="logout.php" style="display:block";>Logout</a>
+</div>
+</body>
+
+</html>
